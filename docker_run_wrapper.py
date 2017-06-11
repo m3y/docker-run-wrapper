62eac92cb75a6ed63ede4de0fc7cb401f7db9a77
m3y <ma3ya.ozw+github@gmail.com>
2017-06-11 11:34:52 +0900

    first commit

---
+++ docker_run_wrapper.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+docker run wrapper.
+'''
+
+import os
+import subprocess
+import sys
+
+
+DEFAULT_CONFIG_FILEPATH = '~/.config/drw/config.toml'
+VOLUME_NAME = '/drw/'
+
+
+def config_filepath():
+    '''
+    設定ファイルパスの取得
+    '''
+    filepath = os.path.expanduser(DEFAULT_CONFIG_FILEPATH)
+    if 'DRW_CONFIG_FILEPATH' in os.environ:
+        filepath = os.environ['DRW_CONFIG_FILEPATH']
+
+    if not os.path.isfile(filepath):
+        raise ValueError('not found.')
+
+    return filepath
+
+
+def parse(line):
+    '''
+    toml ファイルのパース
+    '''
+    return [column.strip().strip('\"') for column in line.split('=')]
+
+
+def correct_format(lines):
+    '''
+    設定ファイルの型式確認
+    '''
+    return len([x for x in lines if x != ""]) >= 2
+
+
+def load_config():
+    '''
+    設定の読み込み
+    '''
+    with open(config_filepath(), 'r') as f:
+        return {
+            parse(i)[0]: parse(i)[1]
+            for i in f if correct_format(parse(i))
+        }
+
+
+def exists_docker():
+    '''
+    docker コマンドの存在確認
+    '''
+    try:
+        subprocess.check_call('/usr/bin/which docker > /dev/null', shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def construct_command(image, command_name, params):
+    '''
+    docker command の構築
+    '''
+    template = 'docker run --rm -it -v $(pwd):{} {} {} {}'
+    return template.format(VOLUME_NAME, image, command_name, ' '.join(params))
+
+
+def run_command(command_string):
+    '''
+    コマンドの実行
+    '''
+    subprocess.call(command_string, shell=True)
+
+
+def with_volumepath(param):
+    '''
+    ファイルパスにvolumeのパスを付加
+    '''
+    if os.path.isfile(os.path.abspath(param)):
+        return VOLUME_NAME + param
+    return param
+
+
+def convert_param(params):
+    '''
+    パラメータ変換
+    '''
+    return [with_volumepath(x) for x in params]
+
+
+def main(input_params):
+    '''
+    docker run wrapper.
+    '''
+    if not exists_docker():
+        raise SystemException('Please install the Docker for Mac')
+
+    command_name = input_params[0]
+    params = convert_param(input_params[1:])
+
+    config = load_config()
+    if command_name not in config:
+        raise ValueError('unknown command.')
+
+    command = construct_command(config[command_name], command_name, params)
+    run_command(command)
+
+
+if __name__ == '__main__':
+    argv = sys.argv
+    if (len(argv) < 2):
+        print('Usage:\n\t{} command [option..]'.format(argv[0]))
+        sys.exit(1)
+
+    try:
+        main(argv[1:])
+    except ValueError, e:
+        print(e.args[0])
+        sys.exit(1)
---
+++ test_docker_run_wrapper.py
@@ -0,0 +1,53 @@
+import unittest
+
+from docker_run_wrapper import parse, correct_format, construct_command, with_volumepath
+
+
+class TestDrw(unittest.TestCase):
+
+    def test_parse(self):
+        line = 'python = "python:3"'
+        expected = ['python', 'python:3']
+        actual = parse(line)
+        self.assertEqual(expected, actual)
+
+
+    def test_correct_format_empty_value(self):
+        lines = ['python', '']
+        expected = False
+        actual = correct_format(lines)
+        self.assertEqual(expected, actual)
+
+
+    def test_correct_format_not_enough(self):
+        lines = ['python']
+        expected = False
+        actual = correct_format(lines)
+        self.assertEqual(expected, actual)
+
+
+    def test_construct_command(self):
+        image = 'python:latest'
+        command_name = 'python'
+        params = ['-mdoctest', 'example.py']
+        expected = 'docker run --rm -it -v $(pwd):/drw/ python:latest python -mdoctest example.py'
+        actual = construct_command(image, command_name, params)
+        self.assertEqual(expected, actual)
+
+
+    def test_filepath_with_volumepath(self):
+        param = 'test_docker_run_wrapper.py'
+        expected = '/drw/test_docker_run_wrapper.py'
+        actual = with_volumepath(param)
+        self.assertEqual(expected, actual)
+
+
+    def test_not_filepath_with_volumepath(self):
+        param = 'not_filepath'
+        expected = 'not_filepath'
+        actual = with_volumepath(param)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
